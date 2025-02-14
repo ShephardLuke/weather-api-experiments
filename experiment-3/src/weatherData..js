68c6ce7cb5a05843783d8971613de831b3585105
ShephardLuke <shephardluke@hotmail.com>
2025-02-14 10:51:03 +0000

    Added API key in process env

--- experiment-3/src/weatherData..js
+++ experiment-3/src/weatherData..js
@@ -1,4 +1,6 @@
 export default function WeatherData({location}) {
+    console.log(process.env.REACT_APP_WEATHER_API_KEY)
+    console.log(process.env.NODE_ENV)
     return (
         <p>Weather data for {location}:</p>
     )
